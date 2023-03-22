5d70b14ee58ce089c145bc1a675fc3ff8e143806
Junaid Popalzai <junaidpop12@gmail.com>
2023-03-21 17:22:00 -0700

    Add scoring system and winning message

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     const choices = ["rock","paper","scissors"];
     return choices[Math.floor(Math.random()*3)];
@@ -12,19 +15,23 @@ function playRound(playerSelection, computerSelection){
         case "paper":
             switch(computerSelection){
                 case "rock":
+                    playerScore++;
                     return roundWon;
                 case "paper":
                     return roundTie;
                 case "scissors":
+                    computerScore++;
                     return roundLoss;
             }
             break;
 
-        case "scisscors":
+        case "scissors":
             switch(computerSelection){
                 case "rock":
+                    computerScore++;
                     return roundLoss;
                 case "paper":
+                    playerScore++;
                     return roundWon;
                 case "scissors":
                     return roundTie;
@@ -36,8 +43,10 @@ function playRound(playerSelection, computerSelection){
                 case "rock":
                     return roundTie;
                 case "paper":
+                    computerScore++;
                     return roundLoss;
                 case "scissors":
+                    playerScore++;
                     return roundWon;
             }
             break;
@@ -50,6 +59,9 @@ function game(){
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection,computerSelection));
     }
+    if (playerScore > computerScore){console.log("Winner, Winner Chicken Dinner!");}
+    else if (playerScore == computerScore){console.log("Tie Game, Unlucky!");}
+    else {console.log("Boohoo, You lost!");}
 }
 
 game();
